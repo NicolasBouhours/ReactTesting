154ae57318c3a601e07929bc25a52494a7893a60
NicolasBouhours <nico53960@hotmail.fr>
2017-01-31 19:12:03 +0100

    Advanced unit test

---
+++ test/reducers/comments_test.js
@@ -0,0 +1,13 @@
+import { expect } from '../test_helper'
+import commentReducer from '../../src/reducers/comments'
+import { SAVE_COMMENT } from '../../src/actions/types'
+
+describe('Comments Reducer', () => {
+  it('handle action with unknow type', () => {
+    expect(commentReducer(undefined, {})).to.eql([])
+  })
+  it('handle action with type SAVE_COMMENT', () => {
+    const action = { type: SAVE_COMMENT, payload: 'new comment' }
+    expect(commentReducer([], action)).to.eql(['new comment'])
+  })
+})
